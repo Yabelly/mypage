7d4f135ad30b589a4cf7257a659cae12fb64fd31
jeroen nepveuE <jnepveu88@gmail.com>
2022-05-24 18:19:57 +0200

    minor add on

--- client/src/resume.js
+++ client/src/resume.js
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { projects, experience, skills, languages } from "./cv-data";
 export default function Resume() {
     return (
@@ -14,35 +13,37 @@ export default function Resume() {
                     <div className={rows.project} key={idx}>
                         <h3>{rows.project.name}</h3>
                         <h4>{rows.project.tech}</h4>
-                        <h4>{rows.project.github}</h4>
+                        <a href={rows.project.github}>github</a>
                     </div>
                 ))}
             </secion>
+            <br></br>
             <secion id="experience">
                 {experience.map((rows, idx) => (
                     <div className={rows.time} key={idx}>
                         <h3>{rows.job}</h3>
-                        <h4>{rows.time}</h4>
-                        <h4>{rows.description}</h4>
+                        <h4>time: {rows.time}</h4>
+                        <h4>description: {rows.description}</h4>
                     </div>
                 ))}
             </secion>
+            <br></br>
             <secion id="skillset">
                 {skills.map((rows, idx) => (
                     <div key={idx}>
-                        <h3>{rows.programming}</h3>
-                        <h4>{rows.frameworks}</h4>
-                        <h4>{rows.databases}</h4>
-                        <h4>{rows.tools}</h4>
+                        <h3>languages: {rows.programming}</h3>
+                        <h4>frameworks: {rows.frameworks}</h4>
+                        <h4>databases: {rows.databases}</h4>
+                        <h4>tools: {rows.tools}</h4>
                     </div>
                 ))}
             </secion>
+            <br></br>
             <secion id="languages">
                 {languages.map((rows, idx) => (
                     <div key={idx}>
-                        <h4>{rows.language}</h4>
-                        <h3>{rows.level}</h3>
-                        <br></br>
+                        <h3>{rows.language}</h3>
+                        <h4>{rows.level}</h4>
                     </div>
                 ))}
             </secion>
