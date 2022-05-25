d4908f03ae855286e517df2b9dc63ac136edab74
jeroen nepveuE <jnepveu88@gmail.com>
2022-05-25 17:08:18 +0200

    about me draft 1 done

--- client/src/about.js
+++ client/src/about.js
@@ -5,44 +5,26 @@ export default function About() {
 
             <section>
                 <p>
-                    In 2004 I started climbing in my local gym in the Netherlands, I wasn't very naturally gifted for it but a good hobby nontheless. two years later in 2006 I was invited to set a climbing route. Little did I know this would become my primary hobby above climbing. 
-                </p>
-                <p>
-                    Doggo ipsum very taste wow pupper wow such tempt heckin good
-                    boys and girls many pats the neighborhood pupper, very taste
-                    wow noodle horse heckin good boys and girls very taste wow.
-                    lotsa pats pupperino doggo. wow very biscit adorable doggo
-                    super chub. Ur givin me a spook waggy wags doge many pats
-                    puggo such treat, pupper long doggo smol shoober. Boof
-                    borking doggo fluffer shibe long bois super chub, heckin
-                    angery woofer doggorino floofs. very hand that feed shibe
-                    mlem tungg. H*ck wrinkler vvv extremely cuuuuuute much ruin
-                    diet big ol, maximum borkdrive corgo blep shoob long water
-                    shoob, such treat you are doing me the shock aqua doggo
-                    pats. Very good spot pupper ur givin me a spook borkf woofer
-                    long woofer boofers, shoob stop it fren fluffer long bois
-                    very good spot. Boof bork pupper fluffer stop it fren corgo,
-                    blop many pats heckin good boys and girls maximum borkdrive.
-                    Doggo very taste wow long woofer ur givin me a spook heckin
-                    angery woofer, fat boi heckin good boys and girls sub
-                    woofer, much ruin diet borkf thicc. Much ruin diet long
-                    doggo doggo, wow very biscit. Heck clouds woofer long
-                    woofer, borking doggo shoober. Long water shoob porgo what a
-                    nice floof borking doggo super chub, the neighborhood pupper
-                    I am bekom fat. Puggo boofers pupperino tungg ur givin me a
-                    spook wow very biscit, doing me a frighten doge corgo long
-                    doggo dat tungg tho, doggorino blep doggo wrinkler. You are
-                    doing me a frighten adorable doggo wow very biscit aqua
-                    doggo, boofers. Blop big ol ur givin me a spook extremely
-                    cuuuuuute noodle horse boofers, smol lotsa pats stop it fren
-                    boofers. Stop it fren boofers most angery pupper I have ever
-                    seen ruff, puggo stop it fren, dat tungg tho pats. Blep very
-                    jealous pupper wow very biscit heckin, borking doggo heckin
-                    waggy wags extremely cuuuuuute, fluffer vvv. The
-                    neighborhood pupper yapper waggy wags I am bekom fat big ol
-                    pupper heckin, much ruin diet vvv shibe. Shooberino very
-                    taste wow such treat fat boi heckin good boys long bois very
-                    good spot, woofer snoot wrinkler very good spot.
+                    In 2004 I started climbing in my local gym in the
+                    Netherlands, I wasn't very naturally gifted for it but a
+                    good hobby nontheless. two years later in 2006 I was invited
+                    to set a climbing route. Little did I know this would become
+                    the primary focus of my adult working career. In the
+                    following years I started to study for a bacholor in Social
+                    Work at Saxion applied University in Enschede. Although an
+                    interesting study I ended up climbing and routesetting most
+                    of the time. In 2011 a bouldering gym opened in Enschede
+                    which took a large portion of my time. Eventually in 2015 I
+                    made the decision to go full-time as a routesetter and
+                    joined the Beta-routesetting collective. From then on I
+                    spende the next years being a freelance routesetter
+                    primarily focussed around bouldering. In these years I ended
+                    up setting everything possible in the space of climbing with
+                    the exeption of a Worldcup: circuits, competitions from
+                    local to international, teamevents, teaching climbing & setting, masterclasses, gymadvising.
+                    
+                    In 2020 I moved to Berlin and lowered the amount of routesetting mostly thanks to the Covid pandamic. In my time off I decided to turn my attention to web development. That resulted with me undergoing a three month full-time bootcamp at Spiced Academy.
+                    This was extremely challeging for me. Sitting behind a desk and thinking in abstract terms was quite the switch from the creative job that is routesetting. 
                 </p>
             </section>
         </>
