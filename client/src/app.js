0ffbb5306adf84657365e50199fe3eba6596bf14
jeroen nepveuE <jnepveu88@gmail.com>
2022-05-25 16:11:45 +0200

    started with css

--- client/src/app.js
+++ client/src/app.js
@@ -13,18 +13,25 @@ export default function App() {
                     <Route>
                         <Header></Header>
                     </Route>
-                    <Route path="/about">
-                        <About></About>
-                    </Route>
-                    <Route path="/resume">
-                        <Resume></Resume>
-                    </Route>
-                    <Route path="/blog">
-                        <Blog></Blog>
-                    </Route>
-                    <Route path="/contact">
-                        <Contact></Contact>
-                    </Route>
+                    <div id="content">
+                        <div id="sidebar-left"></div>
+                        <section id="middle">
+                            <Route path="/about">
+                                <About></About>
+                            </Route>
+                            <Route path="/resume">
+                                <Resume></Resume>
+                            </Route>
+                            <Route path="/blog">
+                                <Blog></Blog>
+                            </Route>
+                            <Route path="/contact">
+                                <Contact></Contact>
+                            </Route>
+                        </section>
+
+                        <div id="sidebar-right"></div>
+                    </div>
                 </BrowserRouter>
 
                 <footer>Concept Setting</footer>
