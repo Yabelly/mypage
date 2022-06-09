5408023cd78b8b95b3784ba92368c84f9f3b5eac
jeroen nepveuE <jnepveu88@gmail.com>
2022-06-09 17:22:12 +0200

    contact page 1st draft

--- client/src/contact.js
+++ client/src/contact.js
@@ -1,8 +1,52 @@
+import { useState } from "react";
 export default function Contact() {
+    const [email, setEmail] = useState();
+    const [message, setMessage] = useState();
+
+    function emailInput({ target }) {
+        setEmail(target.email);
+    }
+
+    function messageInput({ target }) {
+        setMessage(target.message);
+    }
+
+    function sendMessage(e) {
+        e.preventDefault();
+
+        fetch("/api/message", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, message }),
+        })
+            .then((resp) => resp.json())
+            .then((succesStatus) => {
+                console.log("succesStatus: ", succesStatus);
+            });
+    }
+
     return (
         <>
             <section>
-                <div>Jeroen Nepveu</div>
+                <div>Send message to me</div>
+                <br></br>
+                <div>enter your email so I write you back</div>
+                <input
+                    name="email"
+                    type="email"
+                    placeholder="example@email.com"
+                    onChange={(e) => emailInput(e)}
+                ></input>
+                <br></br>
+                <input
+                    name="message"
+                    type="text"
+                    placeholder="Add your question here"
+                    onChange={(e) => messageInput(e)}
+                ></input>
+                <button onClick={(e) => sendMessage(e)}>send message</button>
             </section>
         </>
     );
