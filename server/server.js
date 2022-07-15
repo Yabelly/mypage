4108ba6e39ce92aa416b4f774af2de1f7ad78930
jeroen nepveuE <jnepveu88@gmail.com>
2022-07-15 15:09:33 +0200

    working on secret

--- server/server.js
+++ server/server.js
@@ -1,15 +1,37 @@
+// dependencies below
 const express = require("express");
 const app = express();
 const compression = require("compression");
 const path = require("path");
 const db = require("./database/db");
 
-
+// dependencies above
+// middleware below
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
 app.use(express.json());
+
+let sessionSecret;
+
+if (process.env.NODE_ENV == "production") {
+    sessionSecret = process.env.SESSION_SECRET;
+} else {
+    sessionSecret = require("./secrets").SESSION_SECRET;
+}
+
+if (process.env.NODE_ENV == "production") {
+    app.use((req, res, next) => {
+        if (req.headers["x-forwarded-proto"].startsWith("https")) {
+            return next();
+        }
+        res.redirect(`https://${req.hostname}${req.url}`);
+    });
+}
+
+// middleware above
+// requests below
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "..", "client", "index.html"));
@@ -25,6 +47,9 @@ app.post("/api/message", async (req, res) => {
         res.json({ succesStatus: false });
     }
 });
+
+// requests above
+// server PORT below
 
 app.listen(process.env.PORT || 3001, function () {
     console.log("I'm listening.");
